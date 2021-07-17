788f371930c0f83b1993acaec74845d7b6b0a816
nguyenlehuynhduc <huynhduc.nguyenle@gmail.com>
2021-07-17 08:22:18 +0700

    nguyenlehuynhduc/ChatRoom

--- phuongtrinhbac2/lib/phuongtrinhbac2.ex
+++ phuongtrinhbac2/lib/phuongtrinhbac2.ex
@@ -7,7 +7,11 @@ defmodule Phuongtrinhbac2 do
     if d<0 do
       "phuong trinh vo nghiem"
     else
-
+      x1=(-b+:math.sqrt(b*b - 4*a*c))/(2*a)
+      x2=(-b-:math.sqrt(b*b - 4*a*c))/(2*a)
+    if d=0 do
+      x1=x2=-(b/2*a)
+    end
     end
   end
 end
