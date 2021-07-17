95271358c24a12b4928ef6eb7fdce7f1cdc69e4d
nguyenlehuynhduc <huynhduc.nguyenle@gmail.com>
2021-07-17 07:55:52 +0700

    commit test

--- fib/lib/fib.ex
+++ fib/lib/fib.ex
@@ -1,17 +1,5 @@
 defmodule Fib do
-  @moduledoc """
-  Documentation for `Fib`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Fib.hello()
-      :world
-
-  """
   def hello do
     :world
   end
