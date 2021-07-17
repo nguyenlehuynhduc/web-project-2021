c7d079526262ec8cec2dd5816993af71f6ca68be
nguyenlehuynhduc <huynhduc.nguyenle@gmail.com>
2021-07-17 08:42:13 +0700

    - something.

--- phuongtrinhbac2/lib/phuongtrinhbac2.ex
+++ phuongtrinhbac2/lib/phuongtrinhbac2.ex
@@ -3,13 +3,14 @@ defmodule Phuongtrinhbac2 do
     :world
   end
   #d=b^2x4ac
-  def bac2(a, b, c, d) do
+  # tfvgybnhujimko
+  def bac2(a, b, c) do
     if d<0 do
       "phuong trinh vo nghiem"
     else
       x1=(-b+:math.sqrt(b*b - 4*a*c))/(2*a)
       x2=(-b-:math.sqrt(b*b - 4*a*c))/(2*a)
-    if d=0 do
+    if d==0 do
       x1=x2=-(b/2*a)
     end
     end
