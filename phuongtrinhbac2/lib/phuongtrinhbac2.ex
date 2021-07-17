4976676a669ab706e1d791ecb59b903d3f8303a9
nguyenlehuynhduc <huynhduc.nguyenle@gmail.com>
2021-07-17 07:50:51 +0700

    commit_001

---
+++ phuongtrinhbac2/lib/phuongtrinhbac2.ex
@@ -0,0 +1,14 @@
+defmodule Phuongtrinhbac2 do
+  def hello do
+    :world
+  end
+  #d=b^2x4ac
+  def bac2(a, b, c, d) do
+    if d<0 do
+      "phuong trinh vo nghiem"
+    else
+      x1=(-b+:math.sqrt(square))/2*a
+      x2=()
+    end
+  end
+end
